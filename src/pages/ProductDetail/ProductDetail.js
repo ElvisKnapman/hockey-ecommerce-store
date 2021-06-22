39e753fe60c95707553cab62bdb31de7467dd065
Elvis Knapman <elvis@elvisknapman.com>
2021-06-22 16:22:02 -0400

    WIP: ProductDetail component and styles

---
+++ src/pages/ProductDetail/ProductDetail.js
@@ -0,0 +1,31 @@
+import { useParams } from 'react-router-dom';
+import useGetProduct from '../../hooks/useGetProduct';
+
+// get type from the url string
+const type = window.location.pathname.split('/')[1];
+
+const ProductPage = (props) => {
+  const params = useParams();
+  let { productId: id } = params;
+  // convert product id to number
+  id = Number(id);
+  const [product, error] = useGetProduct(type, id);
+
+  console.log('the type', type);
+
+  console.log(product);
+  if (error.isError) {
+    return (
+      <div>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>Product Page</h1>
+    </div>
+  );
+};
+
+export default ProductPage;
