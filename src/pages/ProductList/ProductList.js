095d2338d0f8fed6f6d3745ea59f7e72d88d3ebd
Elvis Knapman <elvis@elvisknapman.com>
2021-06-22 16:20:15 -0400

    removed data file import and used redux state to replace it

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,17 +1,18 @@
 import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import ProductListItem from '../../components/ProductListItem/ProductListItem';
-
-import productData from '../../data/product-data';
 
 import classes from './ProductList.module.css';
 
 const ProductList = (props) => {
   const location = useLocation();
   const { pathname: route } = location;
+  const products = useSelector((state) => state.items);
+  console.log('the products', products);
   // remove slash
   const itemKey = route.substring(1);
   // get items for the specific route
-  const items = productData[itemKey];
+  const items = products[itemKey];
 
   return (
     <main className={classes['list-container']}>
