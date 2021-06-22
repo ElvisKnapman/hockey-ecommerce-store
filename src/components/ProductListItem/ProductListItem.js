14528d43d0edc4f6f41f56306a7545dfeac7090c
Elvis Knapman <elvis@elvisknapman.com>
2021-06-22 13:29:28 -0400

    imported Link and useLocation, linked to product page

--- src/components/ProductListItem/ProductListItem.js
+++ src/components/ProductListItem/ProductListItem.js
@@ -1,16 +1,21 @@
+import { Link, useLocation } from 'react-router-dom';
 import classes from './ProductListItem.module.css';
 
 const ProductListItem = (props) => {
-  const { name, price, image } = props.item;
+  const location = useLocation();
+  const { pathname } = location;
+  const { id, name, price, image } = props.item;
   return (
-    <div className={classes['product-item']}>
-      <div className={classes['product-info']}>
-        <img className={classes['product-image']} src={image} alt="" />
-        <span className={classes['price-text']}>${price}</span>
-        <span className={classes['product-name-text']}>{name}</span>
-        <button className={classes['btn']}>View Item</button>
+    <Link to={`${pathname}/${id}`}>
+      <div className={classes['product-item']}>
+        <div className={classes['product-info']}>
+          <img className={classes['product-image']} src={image} alt="" />
+          <span className={classes['price-text']}>${price}</span>
+          <span className={classes['product-name-text']}>{name}</span>
+          <button className={classes['btn']}>View Item</button>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
