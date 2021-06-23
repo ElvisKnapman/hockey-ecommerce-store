93be903cc77b870e853086c8e23ff7649f623cb2
Elvis Knapman <elvis@elvisknapman.com>
2021-06-23 13:40:23 -0400

    bug fix and jsx structure changes
    add type as state to make sure useGetProduct gets the type to be able to fetch product.
    
    add markup for add to cart features

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,30 +1,69 @@
 import { useParams } from 'react-router-dom';
+import { useState, useCallback } from 'react';
+import ProductOptions from '../../components/ProductOptions/ProductOptions';
+import AddToCart from '../../components/AddToCart/AddToCart';
 import useGetProduct from '../../hooks/useGetProduct';
 
+import classes from './ProductDetail.module.css';
+
 // get type from the url string
-const type = window.location.pathname.split('/')[1];
 
 const ProductDetail = (props) => {
   const params = useParams();
   let { productId: id } = params;
   // convert product id to number
   id = Number(id);
+  const [type, setType] = useState(
+    () => window.location.pathname.split('/')[1]
+  );
   const [product, error] = useGetProduct(type, id);
+  const [size, setSize] = useState(null);
 
   console.log('the type', type);
+  console.log('the size state in parent product details', size);
 
-  console.log(product);
-  if (error.isError) {
+  const handleSizeChange = useCallback((sizeData) => {
+    setSize(sizeData);
+  }, []);
+
+  const handleAddToCart = (cartData) => {
+    // dispatch to cart
+  };
+
+  // if there was an error retrieving product
+  if (error.hasError) {
     return (
       <div>
         <p>{error.message}</p>
       </div>
     );
   }
+
+  // if product hasn't loaded yet
+  if (!product) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
-    </div>
+    <main className={classes['product-container']}>
+      <div className={classes['product-info-container']}>
+        <h1 className={classes['product-title']}>{product.name}</h1>
+        <img
+          className={classes['product-image']}
+          src={product.image}
+          alt={product.name}
+        />
+      </div>
+      <div className={classes['product-options-container']}>
+        <span className={classes['product-price']}>${product.price}</span>
+        <ProductOptions type={type} onSizeChange={handleSizeChange} />
+        <AddToCart onAddToCart={handleAddToCart} />
+      </div>
+    </main>
   );
 };
 
