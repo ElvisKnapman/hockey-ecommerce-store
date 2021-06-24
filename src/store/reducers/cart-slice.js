465adaf7eb33f1ecba25fa6406f020db5c304e81
Elvis Knapman <elvis@elvisknapman.com>
2021-06-24 11:40:39 -0400

    implement add item cart functionality
    write addItem() reducer function that adds to the quantity of an item already in the cart, or adds the item to the cart if not already present.
    
    import useDispatch and addItem cart action to ProductDetail page. Dispatch the action in handleAddToCart function with the constructed item to be added to cart as the payload

--- src/store/reducers/cart-slice.js
+++ src/store/reducers/cart-slice.js
@@ -6,12 +6,42 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addItem: (state, action) => {},
+    addItem: (state, action) => {
+      // payload shape: id, name, price, options, quantity //
+
+      // check if item is already in cart..
+      const cartItem = state.cart.find((item) => item.id === action.payload.id);
+
+      let updatedCart;
+      // if it is, update the quantity
+      if (cartItem) {
+        updatedCart = state.cart.map((item) => {
+          if (item.id === cartItem.id) {
+            return {
+              ...item,
+              quantity: item.quantity + action.payload.quantity,
+            };
+          }
+          return item;
+        });
+      } else {
+        // if item not already in cart, add it
+        updatedCart = [...state.cart, action.payload];
+      }
+
+      // update the cart total price
+      const updatedPrice = updatedCart.reduce((total, currItem) => {
+        return total + currItem.price * currItem.quantity;
+      }, 0);
+
+      // return the updated state
+      return { cart: updatedCart, totalPrice: Number(updatedPrice.toFixed(2)) };
+    },
 
     removeItem: (state, action) => {},
   },
 });
 
-export const { addItem, removeItem } = cartSlice.actions;
+export const cartActions = cartSlice.actions;
 
 export default cartSlice.reducer;
